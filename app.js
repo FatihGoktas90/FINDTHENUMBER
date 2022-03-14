2cc57d462648646d82c2926903fb1f37eb159c8b
Fatih <goktasfatih90@gmail.com>
2022-03-15 00:05:40 +0300

    updated

---
+++ app.js
@@ -0,0 +1,10 @@
+let enteredInput=document.querySelector(".guess-input");
+let enteredInputBtn=document.querySelector(".btn");
+let remainCount=document.querySelector(".remainCount");
+let footerBtn=document.querySelector(".footer-btn");
+
+//function 
+
+
+//add event listener
+
